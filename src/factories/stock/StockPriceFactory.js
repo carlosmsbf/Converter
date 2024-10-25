a848e99e1571a90649b605df8bd6235644a18e17
carlosmsbf <carlosmsbf@gmail.com>
2024-10-25 15:25:46 -0300

    Test

--- src/factories/stock/StockPriceFactory.js
+++ src/factories/stock/StockPriceFactory.js
@@ -22,7 +22,7 @@ class StockPriceFactory {
   
           if (data.results && data.results.length > 0) {
             const stock = data.results[0];
-            /*const dividends = data.dividendsData.cashDividends[0];*/
+            const lastDividend = stock.dividendsData?.cashDividends?.[0];
             stockData.push({
               symbol: stock.symbol,
               currentPrice: stock.regularMarketPrice,
@@ -33,7 +33,7 @@ class StockPriceFactory {
               fiftyTwoWeekHigh: stock.fiftyTwoWeekHigh,  // 52-week high
               fiftyTwoWeekLow: stock.fiftyTwoWeekLow,    // 52-week low
               previousClose: stock.regularMarketPreviousClose,  // Yesterday's close price
-              lastDividendValue: stock.dividendsData.cashDividends[0].rate        
+              lastDividendValue: lastDividend ? lastDividend.rate : null       
             });
           } else {
             console.error(`No stock data found for ${symbol}`);
